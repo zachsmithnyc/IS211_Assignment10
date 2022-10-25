f715095ec1f93b0fcaa58b78e06e02a1ac579e10
Zachary Smith <zachary.smith69@spsmail.cuny.edu>
2022-10-25 17:32:02 -0400

    Assignment 10

--- music.sql
+++ music.sql
@@ -1,1 +1,21 @@
+CREATE SCHEMA IF NOT EXISTS music;
+
+CREATE TABLE artists (
+    id INTEGER PRIMARY KEY ASC,
+    artist_name TEXT
+);
+
+CREATE TABLE albums (
+    id INTEGER PRIMARY KEY ASC,
+    album_title TEXT,
+    artist_name TEXT
+);
+
+CREATE TABLE songs(
+    id INTEGER PRIMARY KEY ASC<
+    song_title TEXT,
+    album_title TEXT,
+    track_no INTEGER,
+    track_length INTEGER
+);
 
