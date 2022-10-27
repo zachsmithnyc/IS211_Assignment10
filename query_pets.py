6c4a7e9aba6f7fe7f2caea8f229fce167f06da54
Zachary Smith <zachary.smith69@spsmail.cuny.edu>
2022-10-27 16:28:43 -0400

    Assignment 10 complete

--- query_pets.py
+++ query_pets.py
@@ -1,26 +1,54 @@
 import sqlite3 as lite
 
-def print_data(id):
+DB_PATH = 'pets.db'
+PERSON_QRY = "SELECT first_name, last_name, age FROM person WHERE id=?"
+PETS_QRY = "SELECT p.name, p.breed, p.age, p.dead FROM pet p INNER JOIN person_pet pp ON pp.pet_id = p.id INNER JOIN person ps ON ps.id = pp.person_id WHERE ps.id == ?"
+
+def print_data():
     """
     param: person id
     return: data printout
     """
-    con = lite.connect('pets.db')
+    con = lite.connect(DB_PATH)
+    print('Welcome to the pets database')
 
     with con:
-
+            
         cur = con.cursor()
-        cur.execute("SELECT first_name, last_name, age FROM person WHERE id=?", id)
+        id = int(input("Please enter owner id: "))
+        while id > 0:
+            cur.execute(PERSON_QRY, (id,))
+            row = cur.fetchone()
 
-        row = cur.fetchone()
-        print(row)
+            if row:
+                print(f"{row[2]} year old {row[0]} {row[1]}")
 
-        cur.executescript("SELECT name, breed, age, dead FROM pet WHERE ")
+                cur.execute(PETS_QRY, (id,))
+                records = cur.fetchall()
+        
+                for r in records:
+                    if r[3] == 1:
+                        print(f"Owned {r[0]} a {r[1]} who was {r[2]} years old.")
+                    else:
+                        print(f"Owns {r[0]} a {r[1]} who is {r[2]} years old.")
+            else:
+                print(f"id: {id} does not exist.")
+            
+            id = int(input("Please enter owner id: "))
 
-    if id == None:
-        raise ValueError("That id does not exist")
+        else:
+            print("so long space cowboy")
 
 if __name__ == "__main__":
-    print('Welcome to the pets database')
-    id = input('Please enter an id: ')
-    print_data(id)
+    print_data()
+        
+
+    
+        
+            
+
+        
+        
+            
+
+        
